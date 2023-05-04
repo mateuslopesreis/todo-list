a77f9ed102a4145658713a907f9bec1ab3dd9e24
mateuslopesreis <mateus.reis@estudante.ifms.edu.br>
2023-05-03 23:45:39 -0300

    iniciando construcao do  todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { AppBar, Button, Container, Grid, TextField, Toolbar, Typography, useTheme } from '@mui/material';
 
 const darkTheme = createTheme({
   palette: {
@@ -8,10 +9,38 @@ const darkTheme = createTheme({
 });
 
 function App(){
+  const theme = useTheme()
   return (
     <ThemeProvider theme={darkTheme}>
     <CssBaseline />
-    <h1>TODO LIST</h1>
+    <AppBar position='static'>
+      <Toolbar sx={{
+          paddingTop:theme.spacing(2),
+          paddingBottom:theme.spacing(2),
+          display:'flex',
+          justifyContent:'center',
+          alignItems:'center',
+         '@media all': { 
+          minHeight: 200,
+         },
+      }}>
+        <Typography variant='h5' component="h1">todo</Typography>
+      </Toolbar>
+    </AppBar>
+
+      <main>
+        <Container>
+          <Grid container spacing={theme.spacing(0.5)}>
+              <Grid item xl={10} sm={12}>
+                  <TextField name='task' fullWidth/>
+              </Grid>
+              <Grid item xl={2} sm={12}>
+                  <Button variant='contained' fullWidth>Criar</Button>
+              </Grid>
+          </Grid>
+          </Container>
+      </main>
+
     
    </ThemeProvider>
   )
